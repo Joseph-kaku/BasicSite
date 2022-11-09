3e4a03b4844d1a3436c0cbba15fd48ded78287bf
Joseph-kaku <samkaks18@gmail.com>
2022-11-09 11:44:37 -0700

    update

--- prophets.js
+++ prophets.js
@@ -23,15 +23,21 @@ function displayProphets(item)
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let img = document.createElement('img');
-    let h3 = document.createElement('h3');
+    // let h3 = document.createElement('h3');
+    let dob = document.createElement('h3');
+    let pob = document.createElement('h3');
 
     h2.textContent = item.name + " " + item.lastname;
-    h3.textContent = item.birthdate + " " + item.birthplace;
+    // h3.textContent = item.birthdate + " " + item.birthplace;
+    dob.textContent = item.birthdate;
+    pob.textContent = item.birthplace;
     img.setAttribute('src', item.imageurl);
-    // img.altAttribute('src', h2.textContent + " - " + item.order)
+    img.setAttribute('alt', item.name + ' ' + item.lastname)
 
     card.appendChild(h2);
-    card.appendChild(h3);
+    // card.appendChild(h3);
+    card.appendChild(dob);
+    card.appendChild(pob);
     card.appendChild(img);
     document.querySelector('.cards').appendChild(card);
 }
