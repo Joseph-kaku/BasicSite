8bd047e2881297145875dda421fcbb2ea80cefc0
Joseph-kaku <samkaks18@gmail.com>
2022-11-11 13:51:12 -0700

    images

--- chamber/js/business.js
+++ chamber/js/business.js
@@ -2,5 +2,28 @@ const requestURL = "https://joseph-kaku.github.io/wdd230/data.json"
 
 async function getBusiness(requestURL) 
 {
-    
+    const response = await fetch(requestURL)
+    if (response.ok) 
+    {
+        const jsObject = await response.json();
+        const businesses = jsObject['businesses'];
+        businesses.forEach(displayCards);
+    }
 }
+
+function displayCards(item)
+{
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let img = document.createElement('img');
+
+    h2.textContent = item.name;
+    img.setAttribute('src', item.image);
+
+    card.appendChild(h2);
+    card.appendChild(img);
+
+    document.querySelector('.cards').appendChild(card);
+}
+
+getBusiness(requestURL);
