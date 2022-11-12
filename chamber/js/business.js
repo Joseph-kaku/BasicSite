ab8571a1d8d223b392a4ee760a4bb51ef58689e4
Joseph-kaku <samkaks18@gmail.com>
2022-11-12 12:14:32 -0700

    update

--- chamber/js/business.js
+++ chamber/js/business.js
@@ -11,69 +11,20 @@ async function getBusiness(requestURL)
     }
 }
 
-// function displayCards(item)
-// {
-//     let card = document.createElement('section');
-//     let h2 = document.createElement('h2');
-//     let img = document.createElement('img');
+function displayCards(item)
+{
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let img = document.createElement('img');
 
-//     h2.textContent = item.name;
-//     img.setAttribute('src', item.image);
+    h2.textContent = item.name;
+    img.setAttribute('src', item.image);
 
-//     card.appendChild(h2);
-//     card.appendChild(img);
+    card.appendChild(h2);
+    card.appendChild(img);
 
-//     document.querySelector('.cards').appendChild(card);
-// }
-
-// getBusiness(requestURL);
-
-function displayBusinesses(business) {
-    // Create a section/card
-    let card = document.createElement("section");
-    let biznesName = document.createElement("p");
-    let biznesAddress = document.createElement("p");
-    let biznesPhone = document.createElement("p");
-    let biznesWebsite = document.createElement("a");
-    let biznesLogo = document.createElement("img");
-
-    // Update the textContent of each new element
-    biznesName.textContent = `${business.name}`;
-    biznesName.setAttribute("id", "business-name");
-    biznesAddress.textContent = `${business.address}`;
-    biznesPhone.textContent = `${business.phone}`;
-    biznesWebsite.textContent = `${business.website}`;
-    biznesWebsite.setAttribute('href', business.website);
-    biznesLogo.setAttribute("src", business.image);
-    biznesLogo.setAttribute("alt", `${business.name} logo`);
-    
-    // Appened list items in p-tags into the section/card
-    card.appendChild(biznesLogo)
-    card.appendChild(biznesName);
-    card.appendChild(biznesAddress);
-    card.appendChild(biznesPhone);
-    card.appendChild(biznesWebsite);
-
-    // Append card to the existing div.
-    document.querySelector("div.cards").appendChild(card);
-}
-
-const gridView = document.querySelector("#directory-card");
-const listView = document.querySelector("#directory-list");
-const currentDivView = document.querySelector(".cards");
-
-gridView.addEventListener('click', togridView);
-
-function togridView() {
-    currentDivView.classList.remove("list");
-    currentDivView.classList.add("cards");
-}
-
-listView.addEventListener('click', tolistView);
-
-function tolistView() {
-    currentDivView.classList.remove("cards");
-    currentDivView.classList.add("list");
+    document.querySelector('.cards').appendChild(card);
 }
 
 getBusiness(requestURL);
+
