555bc6a66b467947e702b32b7a7ef20aa7d1dd13
Joseph-kaku <samkaks18@gmail.com>
2022-11-12 12:18:33 -0700

    started css in small view

--- chamber/js/business.js
+++ chamber/js/business.js
@@ -7,7 +7,7 @@ async function getBusiness(requestURL)
     {
         const jsObject = await response.json();
         const businesses = jsObject['businesses'];
-        businesses.forEach(displayBusinesses);
+        businesses.forEach(displayCards);
     }
 }
 
@@ -15,13 +15,13 @@ function displayCards(item)
 {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
-    let img = document.createElement('img');
+    // let img = document.createElement('img');
 
     h2.textContent = item.name;
-    img.setAttribute('src', item.image);
+    // img.setAttribute('src', item.image);
 
     card.appendChild(h2);
-    card.appendChild(img);
+    // card.appendChild(img);
 
     document.querySelector('.cards').appendChild(card);
 }
