94ed1c83805bcff420d8225be2dd87569936eb5e
Joseph-kaku <samkaks18@gmail.com>
2022-11-14 22:37:24 -0700

    api practice

--- lesson10/weather-api.js
+++ lesson10/weather-api.js
@@ -3,5 +3,14 @@ const getWeather = async () => {
     const response = await fetch(apiURL);
     const jsObject = await response.json();
     console.log(jsObject);
+    document.querySelector('#current-temp').textContent = jsObject.main.temp;
   };
+
+// const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+// const desc = jsObject.weather[0].description;
+// document.querySelector('#icon-src').textContent = iconsrc;
+// document.querySelector('#weathericon').setAttribute('src', iconsrc);
+// document.querySelector('#weathericon').setAttribute('alt', desc);
+// document.querySelector('figcaption').textContent = desc;
+
 getWeather();
