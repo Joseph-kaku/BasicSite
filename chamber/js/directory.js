a8857bce1189ad5dbf42fdf4806b10220cdabecd
Joseph-kaku <samkaks18@gmail.com>
2022-11-15 12:41:16 -0700

    heavy changes made to the css, js and html

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -14,13 +14,10 @@ async function getBusiness(requestURL)
 function displayCards(item)
 {
     let card = document.createElement('section');
-    // let businessname = document.createElement('h1');
+    let businessname = document.createElement('h3');
     let addy = document.createElement('p');
     let phone = document.createElement('p')
     let web  = document.createElement('p');
-
-    let tr = document.createElement('tr');
-    let td = document.createElement('td');
     // let img = document.createElement('img');
 
     web.innerHTML = `<a href="${item.website}">${item.website}</a>`
@@ -28,20 +25,45 @@ function displayCards(item)
     // img.setAttribute('src', item.image);
     // img.setAttribute('alt', item.name + 'img');
 
+    businessname.textContent = item.name;
     addy.textContent = item.address;
     phone.textContent = item.phone;
 
+    card.appendChild(businessname);
     card.appendChild(addy);
     card.appendChild(phone);
     card.appendChild(web);
 
-    tr.innerHTML = `<td>${item.name}</td><td>${item.address}</td><td>${item.phone}</td><td><a href=${item.website}>${item.website}</a></td>`
+   
 
 
     document.querySelector('.cards').appendChild(card);
-    document.querySelector('.list').appendChild(list);
 }
 
 
+
 getBusiness(requestURL);
+
+
+
+const gridView = document.querySelector("#d-card");
+const listView = document.querySelector("#d-list");
+
+const currentDivView = document.querySelector(".cards");
+
+gridView.addEventListener('click', togridView);
+
+function togridView() {
+    currentDivView.classList.remove("list");
+    currentDivView.classList.add("cards");
+}
+
+listView.addEventListener('click', tolistView);
+
+function tolistView() {
+    currentDivView.classList.remove("cards");
+    currentDivView.classList.add("list");
+}
+
+
 
