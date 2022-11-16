adc419613705938bd41e1c3ad3672ac5a2bc454b
Joseph-kaku <samkaks18@gmail.com>
2022-11-16 10:52:51 -0700

    api practice done

--- lesson10/weather-api.js
+++ lesson10/weather-api.js
@@ -3,14 +3,17 @@ const getWeather = async () => {
     const response = await fetch(apiURL);
     const jsObject = await response.json();
     console.log(jsObject);
-    document.querySelector('#current-temp').textContent = jsObject.main.temp;
+  // to fixed() to specify decima points
+    document.querySelector('#current-temp').textContent = Math.round((jsObject.main.temp - 273.15) * 1.8 + 32);
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
   };
+  
 
-// const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-// const desc = jsObject.weather[0].description;
-// document.querySelector('#icon-src').textContent = iconsrc;
-// document.querySelector('#weathericon').setAttribute('src', iconsrc);
-// document.querySelector('#weathericon').setAttribute('alt', desc);
-// document.querySelector('figcaption').textContent = desc;
 
 getWeather();
+
