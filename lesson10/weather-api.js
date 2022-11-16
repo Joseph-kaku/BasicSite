e26fa6c05a7eb334df8353e3ddcf880ae080e1cf
Joseph-kaku <samkaks18@gmail.com>
2022-11-16 11:01:17 -0700

    update

--- lesson10/weather-api.js
+++ lesson10/weather-api.js
@@ -1,4 +1,4 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5861897&appid=b1bec77cc204ab32a7422058dfa36545";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5605242&appid=b1bec77cc204ab32a7422058dfa36545";
 const getWeather = async () => {
     const response = await fetch(apiURL);
     const jsObject = await response.json();
